6daf87afd3b97e8d45491bac8b7cfd6eb446b551
william-rauwens-oiver <william.rauwens-oliver@laplateforme.io>
2024-01-30 16:40:15 +0100

    add job07.py

--- jour02/job07.py
+++ jour02/job07.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-class salarie:
+class SalarieManager:
 
     def __init__(self, host, user, password, database):
         self.conn = mysql.connector.connect(
@@ -30,17 +30,40 @@ class salarie:
         result = self.cursor.fetchall()
         return result
 
+    def met_a_jour_employe(self, employe_id, nom, prenom, salaire, id_service):
+        query = "UPDATE employe SET nom = %s, prenom = %s, salaire = %s, id_service = %s WHERE id = %s"
+        values = (nom, prenom, salaire, id_service, employe_id)
+        self.cursor.execute(query, values)
+        self.conn.commit()
+
+    def supprime_employe(self, employe_id):
+        query = "DELETE FROM employe WHERE id = %s"
+        values = (employe_id,)
+        self.cursor.execute(query, values)
+        self.conn.commit()
+
+    def obtenir_employe_par_id(self, employe_id):
+        query = "SELECT * FROM employe WHERE id = %s"
+        values = (employe_id,)
+        self.cursor.execute(query, values)
+        result = self.cursor.fetchone()
+        return result
+
     def connexion_ferme(self):
         self.conn.close()
 
-salarie_manager = salarie("localhost", "root", "willy", "metier")
+salarie_manager = SalarieManager("localhost", "root", "willy", "metier")
 
-salarie_manager.cree_employe("NouveauNom", "NouveauPrenom", 3500.50, 2)
+salarie_manager.cree_employe("Paul", "Franc", 3500.50, 2)
+
+salarie_manager.met_a_jour_employe(1, "Bert", "François", 4000.0, 3)
 
 result_above_3000 = salarie_manager.lis_employe(3000)
 print("Employés avec salaire > 3000 :", result_above_3000)
 
 result_with_service = salarie_manager.lis_service_et_employe()
 print("Employés avec leur service respectif :", result_with_service)
+
+salarie_manager.supprime_employe(8)
 
 salarie_manager.connexion_ferme()
